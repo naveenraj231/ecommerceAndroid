9aaf3ba409d007f05eba1a896b222d8a11769ad8
simrankumarisah <simran.kumari@coviam.com>
2020-01-23 16:13:47 +0530

    login done

--- app/src/main/java/com/example/myapplication/loginsignupactivity/SignupActivity.java
+++ app/src/main/java/com/example/myapplication/loginsignupactivity/SignupActivity.java
@@ -2,15 +2,21 @@ package com.example.myapplication.loginsignupactivity;
 
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.provider.SyncStateContract;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,7 +25,9 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -78,6 +86,7 @@ public class SignupActivity extends AppCompatActivity {
         imageView=(ImageView)findViewById(R.id.imageView);
         select=(Button)findViewById(R.id.buttonChoose);
         upload=(Button)findViewById(R.id.buttonUpload);
+
 
 
 //        select.setOnClickListener((View.OnClickListener) this);
@@ -172,116 +181,6 @@ public class SignupActivity extends AppCompatActivity {
 
 
 
-
-//
-//    public void uploadMultipart() {
-//
-//        //getting the actual path of the image
-//        String path = getPath(filePath);
-//
-//        //Uploading code
-//        try {
-//            String uploadId = UUID.randomUUID().toString();
-//            new MultipartUploadRequest(this, uploadId, SyncStateContract.Constants.UPLOAD_URL)
-//                    .addFileToUpload(path, "image") //Adding file
-//                    .addParameter("name", name) //Adding text parameter to the request
-//                    .setNotificationConfig(new UploadNotificationConfig())
-//                    .setMaxRetries(2)
-//                    .startUpload(); //Starting the upload
-//
-//        } catch (Exception exc) {
-//            Toast.makeText(this, exc.getMessage(), Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//
-//    //method to show file chooser
-//    private void showFileChooser() {
-//        Intent intent = new Intent();
-//        intent.setType("image/*");
-//        intent.setAction(Intent.ACTION_GET_CONTENT);
-//        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-//    }
-//
-//    //handling the image chooser activity result
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-//            filePath = data.getData();
-//            try {
-//                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-//                imageView.setImageBitmap(bitmap);
-//
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//
-//    //method to get the file path from uri
-//    public String getPath(Uri uri) {
-//        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-//        cursor.moveToFirst();
-//        String document_id = cursor.getString(0);
-//        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
-//        cursor.close();
-//
-//        cursor = getContentResolver().query(
-//                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-//                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
-//        cursor.moveToFirst();
-//        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
-//        cursor.close();
-//
-//        return path;
-//    }
-//
-//
-//    //Requesting permission
-//    private void requestStoragePermission() {
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-//            return;
-//
-//        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-//            //If the user has denied the permission previously your code will come to this block
-//            //Here you can explain why you need this permission
-//            //Explain here why you need this permission
-//        }
-//        //And finally ask for the permission
-//        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
-//    }
-//
-//
-//    //This method will be called when the user will tap on allow or deny
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//
-//        //Checking the request code of our request
-//        if (requestCode == STORAGE_PERMISSION_CODE) {
-//
-//            //If permission is granted
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                //Displaying a toast
-//                Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
-//            } else {
-//                //Displaying another toast if permission is not granted
-//                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
-//            }
-//        }
-//    }
-//
-//
-//    @Override
-//    public void onClick(View v) {
-//        if (v == select) {
-//            showFileChooser();
-//        }
-//        if (v == upload) {
-//            uploadMultipart();
-//        }
-//    }
 
 
 
